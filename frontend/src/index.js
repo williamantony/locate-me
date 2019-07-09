03102046c4a8febb032f8b49969d0d9bba2440da
William Antony <william@willsquad.com>
2019-07-08 23:09:02 -0500

    Implemented Redux & React-Router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// Router
+import { BrowserRouter as Router } from 'react-router-dom';
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Reducers from './redux/reducers';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './Reset.css';
+import './index.css';
+
+const store = createStore(Reducers, applyMiddleware(ReduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
