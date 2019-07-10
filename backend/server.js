2484f971671ae43dcd220b31421dfa2489aebffe
Kwaku <kwa2the4@gmail.com>
2019-07-09 22:20:45 -0400

    Implemented server.js (#2)
    * Added a backend to the project with yarn
    
    * added yarn.lock to created gitignore for backend
    
    * added test route to the backend and new installed dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
 const express =  require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+
+const server = express();
+server.use(bodyParser.json());
+server.use(cors());
+morgan("combined");
+
+
+server.get('/', (req, res) => {
+  res.send("Testing")
+})
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`)
+});
