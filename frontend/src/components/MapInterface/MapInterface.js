faa539360228ea7dbdef6ff2b6b8e22a842d21e6
William Antony <william@willsquad.com>
2019-09-09 18:47:25 -0500

    Modified Google Map Styles in MapInterface Component

--- frontend/src/components/MapInterface/MapInterface.js
+++ frontend/src/components/MapInterface/MapInterface.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './MapInterface.css';
+import MapEditor from '../MapEditor/MapEditor';
 
 class MapInterface extends Component {
   constructor(props) {
@@ -8,28 +9,91 @@ class MapInterface extends Component {
     this.map = null;
     this.mapElement = React.createRef();
     this.state = {
-      
+      location: null,
+      mapLoaded: false,
+      mapMarkerSet: false,
     };
   }
 
+  static getDerivedStateFromProps(props, state) {
+    if (!!props.location.updatedAt) {
+      return {
+        location: props.location,
+      };
+    }
+    return null;
+  }
+
   componentDidMount() {
+    const SI = setInterval(() => {
 
-    window.initMap = () => {
+      const { location, mapLoaded } = this.state;
 
-      this.map = new window.google.maps.Map(this.mapElement.current, {
-        center: {lat: 0, lng: 0},
-        zoom: 8,
+      if (!mapLoaded && !!location) {
+        clearInterval(SI);
+        this.createMap();
+      }
+    }, 250);
+  }
+
+  createMap() {
+
+    global.initMap = () => {
+
+      const { latLng } = this.state.location;
+
+      this.map = new global.google.maps.Map(this.mapElement.current, {
+        center: latLng,
+        gestureHandling: 'greedy',
+        zoom: 16,
+        minZoom: 2,
+        maxZoom: 25,
         zoomControl: true,
         zoomControlOptions: {
-          position: window.google.maps.ControlPosition.RIGHT_TOP,
+          position: global.google.maps.ControlPosition.RIGHT_BOTTOM,
         },
         mapTypeControl: false,
         scaleControl: false,
         streetViewControl: false,
         rotateControl: false,
         fullscreenControl: false,
+
+        restriction: {
+          latLngBounds: {
+            north: 80,
+            south: -85,
+            east: 180,
+            west: -180,
+          },
+          strictBounds: true,
+        },
+
+        backgroundColor: '#ffffff',
+        styles: [
+          {
+            featureType: "poi",
+            elementType: "labels.icon",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+        ],
       });
-    
+
+      this.map.setCenter(latLng);
+
+      global.google.maps.event.addListener(this.map, 'center_changed', () => {
+        this.setState({
+          mapMarkerSet: true,
+        });
+      });
+
+      this.setState({
+        mapLoaded: true,
+      });
+
     };
 
     const { REACT_APP_GOOGLE_MAPS_API } = process.env;
@@ -42,17 +106,26 @@ class MapInterface extends Component {
     document.documentElement.appendChild(scriptTag);
 
   }
-
+  
   render() {
     return (
-      <div ref={this.mapElement} className="MapInterface" />
+      <div className="MapInterface">
+        <div className="MapInterface__loader"></div>
+        <div ref={this.mapElement} className="MapInterface__mapElement" data-maploaded={this.state.mapMarkerSet}></div>
+        {
+          this.state.mapLoaded &&
+          <MapEditor map={this.map} />
+        }
+      </div>
     );
   }
 
 }
 
 const mapStateToProps = state => {
-  return {};
+  return {
+    location: state.self.location || {},
+  };
 };
 
 const mapDispatchToProps = {
