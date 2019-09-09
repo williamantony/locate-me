37ee1a6a065518760fd0cc784fb2de41e8cc61e4
William Antony <william@willsquad.com>
2019-09-09 16:40:43 -0500

    Added history & dotenv to the package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Router
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import History from './History';
 // Redux
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import Reducers from './redux/reducers';
-
+import Store from './Store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import dotenv from 'dotenv';
 
 import './Reset.css';
 import './index.css';
 
-const store = createStore(Reducers, applyMiddleware(ReduxThunk));
+dotenv.config();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
+  <Provider store={Store}>
+    <Router history={History}>
       <App />
     </Router>
   </Provider>,
@@ -28,4 +28,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
