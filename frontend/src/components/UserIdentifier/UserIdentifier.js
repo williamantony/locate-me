46e1ce7146385f38ab295d30d7331c0439e203e4
William Antony <william@willsquad.com>
2019-09-09 21:44:46 -0500

    Modified UserIdentifier Component; Converted functional-component to class; This component will request required data related to client from the backend.

--- frontend/src/components/UserIdentifier/UserIdentifier.js
+++ frontend/src/components/UserIdentifier/UserIdentifier.js
@@ -1,11 +1,79 @@
-import React from 'react';
-import uuid from 'uuid/v4';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  createModal,
+  createClientRequest,
+  setClientId,
+  setClientInfo,
+  setClientName,
+  getConnections,
+  getBeaconsList,
+} from '../../redux/actions';
+import UserNameInputModal from '../modals/UserNameInputModal/UserNameInputModal';
 
-const UserIdentifier = () => {
-  const clientId = sessionStorage.getItem('client') || uuid();
-  sessionStorage.setItem('client', clientId);
-  
-  return null;
+class UserIdentifier extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientId: null,
+      name: props.name,
+    };
+    props.getConnections();
+    props.getBeaconsList();
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const { name } = props;
+    if (name !== state.name)
+      return { name };
+    return null;
+  }
+
+  async componentDidMount() {
+    // Create and Set Client ID
+    const client = await createClientRequest();
+    const { clientId } = client;
+    this.props.setClientId(client.clientId);
+    this.props.setClientInfo(client);
+    this.setState({ clientId });
+  }
+
+  componentDidUpdate() {
+    if (!this.state.name) {
+
+      const modalContent = (
+        <UserNameInputModal
+          name={this.state.name}
+          onClick={this.setClientName}
+        />
+      );
+      this.props.createModal('SetUserNameModal', modalContent, true);
+
+    }
+  }
+
+  setClientName = name => {
+    this.props.setClientName(name);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    name: state.self.name || null,
+  };
 };
 
-export default UserIdentifier;
+const mapDispatchToProps = {
+  createModal,
+  setClientId,
+  setClientInfo,
+  setClientName,
+  getConnections,
+  getBeaconsList,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserIdentifier);
