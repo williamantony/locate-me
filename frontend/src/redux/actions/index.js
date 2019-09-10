cd5073af1155c78c6b55b12d18b65351d9779e70
William Antony <william@willsquad.com>
2019-09-09 19:32:45 -0500

    Updated index.js for both actions and reducers

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -1,1 +1,5 @@
 export * from './modal.actions';
+export * from './connections.actions';
+export * from './clients.actions';
+export * from './beacons.actions';
+export * from './theme.actions';
--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,6 +1,14 @@
 import { combineReducers } from 'redux';
 import modal from './modal.reducer';
+import self from './clients.reducer';
+import connections from './connections.reducer';
+import beacons from './beacons.reducer';
+import theme from './theme.reducer';
 
 export default combineReducers({
   modal,
+  connections,
+  beacons,
+  self,
+  theme,
 });
