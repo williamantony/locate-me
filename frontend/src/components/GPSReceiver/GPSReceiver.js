e314c9b585ecba02f1d0e606761225404b6eb48f
William Antony <william@willsquad.com>
2019-09-09 19:12:53 -0500

    Modified GPSReciever to export withLocationTracker and MapInterface

--- frontend/src/components/GPSReceiver/GPSReceiver.js
+++ frontend/src/components/GPSReceiver/GPSReceiver.js
@@ -1,30 +1,16 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client';
+import React from 'react';
 import './GPSReceiver.css';
-import LocationRequestForm from '../LocationRequestForm/LocationRequestForm';
-import LocationSummary from '../LocationSummary/LocationSummary';
+import SocketReceiver from '../SocketReceiver/SocketReceiver';
+import MapInterface from '../MapInterface/MapInterface';
+import withLocationTracker from '../LocationTracker/withLocationTracker';
 
-class GPSReceiver extends Component {
-  constructor(props) {
-    super(props);
-    this.socket = io('http://localhost:5050', {
-      path: '/location',
-    });
-  }
+const GPSReceiver = () => {
+  return (
+    <div className="GPSReceiver">
+      <SocketReceiver />
+      <MapInterface />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="GPSReceiver">
-        <div className="wrapper">
-
-          <LocationRequestForm />
-          <LocationSummary />
-          
-        </div>
-      </div>
-    );
-  }
-
-}
-
-export default GPSReceiver;
+export default withLocationTracker(GPSReceiver);
